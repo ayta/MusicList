d10d6d7ed28a80b29212b74569c00620fac0286d
Mohsen Khadem <mkhadem@ixeee.com>
2018-04-02 08:33:49 -0500

    API and testing setup

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
 const logger = require('morgan');
-
+const LocalStrategy = require('passport-local').Strategy;
+// Models
+const User = require('./models/user');
+// Routers
 const index = require('./routes/index');
 const api = require('./routes/api/index');
+const users = require('./routes/api/users');
+// const expressSession = require('express-session');
+const expressSession = require('express-session')({
+  secret: 'any random string can go here',
+  resave: false,
+  saveUninitialized: false,
+});
 
 const app = express();
+
+// Connect Mongoose
+mongoose.connect('mongodb://localhost/musiclist');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api', api)
+app.use('/api', api);
+app.use('/api/users', users);
+
+// Configure Passport
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
